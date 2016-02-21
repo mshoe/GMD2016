f7c65ff2270f242585998f0e2fcc072a8c648445
Michael Xu <to.michaelxu@gmail.com>
2016-02-21 08:00:05 -0500

    initial commit

---
+++ Assets/Scripts/HaloBeat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HaloBeat : MonoBehaviour {
+
+	public Behaviour halo;
+	public float haloSize;
+
+	void Start () {
+		halo = (Behaviour)GetComponent ("Halo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PowerUp1.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp1 : MonoBehaviour {
+
+	public int time;
+	public bool Clone = false;
+
+	void Start() {
+		time = 0;
+		if (transform.position.x < 5.0f && transform.position.x > -5.0f) {
+			Clone = true;
+		}
+	}
+
+	void Update(){
+		time += 1;
+		if (time == 900 && Clone) {
+			Destroy (gameObject);
+		}
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.CompareTag ("Player")) {
+			col.gameObject.GetComponent<PlayerController> ().speedPower = true;
+			Destroy (gameObject);
+		}
+	}
+}
